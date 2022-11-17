ca4e625cd4006ff73a872abc3a84a705bf1a1e23
Your Name01~ <you@example.com>
2022-11-17 15:46:50 +0300

    Ex001

--- Ex001/Program.cs
+++ Ex001/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 2//
+
+/*
+Console.WriteLine("Введите натуральное число ");
+int x=Convert.ToInt32 ( Console.ReadLine());
+int y=Convert.ToInt32 ( Console.ReadLine());
+if (x > y) Console.WriteLine("то x максимальное число");
+if (y > x) Console.WriteLine("то y максимальное число");
+
+
+//задача 4//
+Console.WriteLine("Узнаем из 3рех чисел максимальное число ");
+int a= 2;
+int b= 3;
+int c= 7;
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max ) max = b;
+if (c > max) max = c;
+Console.Write("max = ");
+Console.WriteLine(max);
+
+
+  //Задача 6//
+  Console.WriteLine("Введите число : ");
+  int a = Convert.ToInt32(Console.Read());
+  if (a % 2 == 0) Console.WriteLine("Четное");
+  else Console.WriteLine("Нечетное");
+  Console.ReadKey();
+
+//Задача 8//
+ static void Main(string[] args)
+    {
+        Console.Write("Write number 1: ");
+        int n = int.(Console.ReadLine());
+        Console.Write("Write number 2: ");
+        int m = int.(Console.ReadLine());
+        for (int i = n; i <= m; i++)
+        {
+            if (i % 2 == 0) 
+            {
+                Console.Write(i + " ");
+            }
+        }
+        Console.ReadKey();
+    }
+}
+*/
